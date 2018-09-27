79fa973ab6a9d2f9a9413aa5f1f79ab4b24c44ae
cshannon3 <crssoccer3@gmail.com>
2018-09-27 15:32:12 -0400

    Made blocks go down and started getting the line clear done

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:tetris_f/controller.dart';
 import 'package:tetris_f/shared.dart';
@@ -36,64 +38,73 @@ class _MyHomePageState extends State<MyHomePage> {
 
   double paddingtop;
   double paddingbottom;
-  int rootposition = 15;
+  int initrootposition = 15;
   int rotationint = 0;
   List<int> boxes2 = [];
   List<int> taken = [];
+  List<List<int>> taken2 = List.generate(12, (i) => []);
+  List<int> filledboxesperrow = new List<int>.generate(12, (i) => 0 );
   List<List<int>> currentShapePositions;
-
+  int currentrootposition;
+  Timer timer;
+  Stopwatch stopwatch = Stopwatch();
   @override
   void initState() {
     paddingtop = 100.0;
+    print(filledboxesperrow);
     paddingbottom = 100.0;
     Random random = new Random();
     currentShapePositions = shapepositions[random.nextInt(shapepositions.length-1)];
-
+    currentrootposition = initrootposition;
     refreshboxes2(Movement.init);
-  }
-  nextbox() {
-    Random random = new Random();
-    currentShapePositions = shapepositions[random.nextInt(shapepositions.length-1)];
-    setState(() {
-      taken.addAll(boxes2);
-      rootposition = 15;
-      rotationint = 0;
+    timer?.cancel(); // cancel old timer if it exists
+    //Start new timer
+    timer = Timer.periodic(Duration(seconds:  1), (Timer timer){
+      setState(() {
+        refreshboxes2(Movement.down);
+      });
     });
+  }
+
+  @override
+  void dispose() {
+    timer?.cancel();
+    super.dispose();
   }
 
   refreshboxes2(Movement m) {
     switch (m) {
       case (Movement.right):
-        if ((currentShapePositions[rotationint][1]+rootposition+1)%10 !=0 ){
-          rootposition +=1;
+        if ((currentShapePositions[rotationint][1]+currentrootposition+1)%10 !=0 ){
+          currentrootposition +=1;
         }
         break;
       case (Movement.left):
-        if ((currentShapePositions[rotationint][0]+rootposition)%10 !=0 ){
-          rootposition -=1;
+        if ((currentShapePositions[rotationint][0]+currentrootposition)%10 !=0 ){
+          currentrootposition -=1;
         }
         break;
       case (Movement.up):
-        if ((currentShapePositions[rotationint][2]+rootposition-10)>0 ){
-          rootposition -=10;
+        if ((currentShapePositions[rotationint][2]+currentrootposition-10)>0 ){
+          currentrootposition -=10;
         }
         break;
       case (Movement.down):
-        if ((currentShapePositions[rotationint][3]+rootposition+10) <120 ){
-          rootposition +=10;
+        if ((currentShapePositions[rotationint][3]+currentrootposition+10) <120 ){
+          currentrootposition +=10;
             bool landed = false;
             currentShapePositions[rotationint].forEach((v) {
-              if (!landed && taken.contains(v+rootposition)){
+              if (!landed && taken.contains(v+currentrootposition)){
                 landed = true;
                 nextbox();
               }
             });
 
-        if (!landed && currentShapePositions[rotationint][3]+rootposition >109){
+        if (!landed && currentShapePositions[rotationint][3]+currentrootposition >109){
           boxes2 = [];
           setState(() {
             currentShapePositions[rotationint].forEach((val) {
-              boxes2.add(val + rootposition);
+              boxes2.add(val + currentrootposition);
             });
           });
           nextbox();
@@ -105,8 +116,8 @@ class _MyHomePageState extends State<MyHomePage> {
         if (currentShapePositions.length> rotationint+1) {
           newrotationint = rotationint+1;
         }
-        if ((currentShapePositions[newrotationint][0]+rootposition+1)%10 != 0
-            && (currentShapePositions[newrotationint][1]+rootposition)%10 !=0 ){
+        if ((currentShapePositions[newrotationint][0]+currentrootposition+1)%10 != 0
+            && (currentShapePositions[newrotationint][1]+currentrootposition)%10 !=0 ){
             setState(() {
               rotationint = newrotationint;
             });
@@ -118,8 +129,48 @@ class _MyHomePageState extends State<MyHomePage> {
     boxes2 = [];
     setState(() {
       currentShapePositions[rotationint].forEach((val) {
-        boxes2.add(val + rootposition);
+        boxes2.add(val + currentrootposition);
       });
+    });
+  }
+
+
+  nextbox() {
+    Random random = new Random();
+    currentShapePositions = shapepositions[random.nextInt(shapepositions.length-1)];
+    setState(() {
+      taken.addAll(boxes2);
+      List<int> checkrepeats = [];
+      boxes2.forEach((val) {
+        if(!checkrepeats.contains(val)) {
+          filledboxesperrow[(val / 10).toInt()] += 1;
+          checkrepeats.add(val);
+        }
+        if (filledboxesperrow[(val/10).toInt()] ==10) {
+          eraserow(filledboxesperrow[(val/10).toInt()]);
+          print((val/10).toInt());
+        }
+      });
+      print(filledboxesperrow);
+      currentrootposition = initrootposition;
+      rotationint = 0;
+    });
+  }
+
+  eraserow(int rownum){
+    filledboxesperrow.removeAt(rownum+1);
+    filledboxesperrow.insert(0, 0);
+    List<int> newtaken = [];
+    taken.forEach((i){
+      if ((i/10).toInt() < rownum+1) {
+        newtaken.add(i + 10);
+      }else if ((i/10).toInt() > rownum+1){
+        newtaken.add(i);
+      }
+    });
+    setState(() {
+      taken = newtaken;
+    print(taken);
     });
   }
   @override
